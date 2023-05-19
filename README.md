2f6a1c7061da1206b6b12e099a25f812523858ed
pj8912 <jpintofun2b@gmail.com>
2023-05-19 21:14:48 +0530

    updated readme, added bardapi

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # bard-php-terminal
 Terminal Chat with Bard using Bard API and PHP
+
+## Requirements
+- `PHP 8.1`
+- `Composer`
+
+## Install
+- Clone
+```bash
+git clone https://github.com/pj8912/bard-php-terminal.git
+```
+## Enter Your Key
+```php
+$_ENV['_BARD_API_KEY'] = "ENTER_YOUR_KEY";
+```
+## Start
+```bash
+php index.php
+```
--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
-require_once 'vendor/autoload.php';use Pj8912\PhpBardApi\Bard;
+require_once 'vendor/autoload.php';
+use Pj8912\PhpBardApi\Bard;
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+header('Content-Type: text/html; charset=utf-8');
 $_ENV['_BARD_API_KEY'] = "ENTER_YOUR_KEY";
 $bard = new Bard();
 while(True){
-  try{
-    echo "\n";
-    $input_text =readline("\U0001F464You: ");
-    $result = $bard->get_answer($input_text);             
-    $content = $result["content"];
-    print("\U0001F916 Bard: ".$content."\n");
-    for ($i = 0; $i < 100; $i++) {
-     echo "*";
-    }
-    echo "\n\n";
-  }
-  catch(Exception $e){
-    echo "err ".$e->getMessage();
-  }
+	try{
+		echo "\n";
+	  	echo "You are chatting with Google's BARD";
+	  	echo "\n";
+    	  	$input_text =readline("\u{1F464} You: ");
+    		$result = $bard->get_answer($input_text);             
+    		$content = $result["content"];
+    		echo "\n";
+    		print("\u{1F916} Bard: ".$content);
+    		echo "\n";
+    		for ($i = 0; $i < 100; $i++) {
+	    	echo "*";
+    		}
+    		echo "\n\n";
+  	}
+	catch(Exception $e){
+		echo "err ".$e->getMessage();
+	}
 }
 ?>
